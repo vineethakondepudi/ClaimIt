6dc6763b7863c412f43daa5c67066420367139b5
gmugada-at-646632670602 <Ganesh.Mugada@americold.com>
2024-12-05 16:34:44 +0530

    changes for footer

--- src/app/features/view-unclaim/view-unclaim.component.ts
+++ src/app/features/view-unclaim/view-unclaim.component.ts
@@ -13,6 +13,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { FooterComponent } from '../../layout/footer/footer.component';
 @Component({
   selector: 'app-view-unclaim',
   standalone: true,
@@ -22,6 +23,7 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     MatIconModule,
     FormsModule,
     MatButtonModule,
+    FooterComponent,
     ConfirmationDialogComponentComponent,
     MatExpansionModule,
     MatDialogModule,
--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { COPYRIGHT_YEAR, COPYRIGHT_OWNER, RIGHTS_RESERVED, ClAIMIT_VERSION } from '../constants/footer.constant';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule,MatCardModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  public year = COPYRIGHT_YEAR
+  public Owner = COPYRIGHT_OWNER
+  public  reserved = RIGHTS_RESERVED
+  public version = ClAIMIT_VERSION
+  public claimitURl = ''
+  public companyInfo = ``;
+  @Input() copyright:boolean=true
 }
