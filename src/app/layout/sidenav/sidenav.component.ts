1744ca87cb802bfc19c65584720cf18434c043b8
gmugada-at-646632670602 <Ganesh.Mugada@americold.com>
2024-12-05 16:36:24 +0530

    changes

--- src/app/layout/sidenav/sidenav.component.ts
+++ src/app/layout/sidenav/sidenav.component.ts
@@ -12,86 +12,90 @@ import { BehaviorSubject } from 'rxjs';
 import { ThemeService } from '../theme.service';
 
 @Component({
-  selector: 'app-sidenav',
-  standalone: true,
-  imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatListModule,
-    RouterModule,
-    MatTooltipModule,
-  ],
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss'],
+    selector: 'app-sidenav',
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        RouterModule,
+        MatTooltipModule,
+    ],
+    templateUrl: './sidenav.component.html',
+    styleUrls: ['./sidenav.component.scss'],
 })
-export class SidenavComponent  {
-  isLoginPage!: boolean;
-  isSideNavStatus: boolean = true;
-  isMobile: boolean = false;
-  profImage =
-    'https://cdn.iconscout.com/icon/free/png-512/free-user-circle-icon-download-in-svg-png-gif-file-formats--contact-profile-ui-vol-9-pack-interface-icons-3005455.png?f=webp&w=256';
+export class SidenavComponent {
+    isLoginPage!: boolean;
+    isSideNavStatus: boolean = true;
+    isMobile: boolean = false;
+    profImage =
+        'https://cdn.iconscout.com/icon/free/png-512/free-user-circle-icon-download-in-svg-png-gif-file-formats--contact-profile-ui-vol-9-pack-interface-icons-3005455.png?f=webp&w=256';
 
-  @ViewChild('snav') sidenav!: MatSidenav;
+    @ViewChild('snav') sidenav!: MatSidenav;
 
-  fillerNav: { name: string; routeLink: string; icon: string }[] = [];
-  private roleSubject = new BehaviorSubject<string | null>(localStorage.getItem('role'));
+    fillerNav: { name: string; routeLink: string; icon: string }[] = [];
+    private roleSubject = new BehaviorSubject<string | null>(localStorage.getItem('role'));
 
-  constructor(
-    private themeService: ThemeService,
-    private breakpointObserver: BreakpointObserver,
-    public router: Router
-  ) {
-    this.breakpointObserver.observe([Breakpoints.Handset]).subscribe((result) => {
-      this.isMobile = result.matches;
-    });
+    constructor(
+        private themeService: ThemeService,
+        private breakpointObserver: BreakpointObserver,
+        public router: Router
+    ) {
+        this.breakpointObserver.observe([Breakpoints.Handset]).subscribe((result) => {
+            this.isMobile = result.matches;
+        });
 
-    this.router.events.subscribe(() => {
-      this.isLoginPage = this.router.url.includes('login');
-    });
+        this.router.events.subscribe(() => {
+            this.isLoginPage = this.router.url.includes('login');
+        });
 
-    this.roleSubject.subscribe((role) => this.setNavigationItems(role));
-  }
-  ngOnInit() {
-    
-  }
-
-  toggleSidenav() {
-    this.isSideNavStatus = !this.isSideNavStatus;
-  }
-
-  toggleTheme(): void {
-    this.themeService.toggleTheme();
-  }
-
-  onSidenavStateChange(event: boolean) {
-    this.isSideNavStatus = event;
-  }
-
-  logout() {
-    localStorage.removeItem('role');
-    this.roleSubject.next(null); // Clear role on logout
-    this.router.navigate(['/login']);
-  }
-
-  setNavigationItems(role: string | null) {
-    if (role === 'admin') {
-      this.fillerNav = [
-        { name: 'Home', routeLink: '/home', icon: 'home' },
-        { name: 'Add Item', routeLink: 'add-item', icon: 'add' },
-        { name: 'Remove/Archive Item', routeLink: 'remove-archive', icon: 'archive' },
-        { name: 'Search', routeLink: 'search', icon: 'search' },
-      ];
-    } else if (role === 'user') {
-      this.fillerNav = [
-        { name: 'Home', routeLink: '/home', icon: 'home' },
-        { name: 'Search and Claim', routeLink: 'search-claim', icon: 'search' },
-        { name: 'View/Unclaim Item', routeLink: 'view-unclaim', icon: 'visibility' },
-      ];
-    } else {
-      this.fillerNav = [];
+        this.roleSubject.subscribe((role) => this.setNavigationItems(role));
     }
-  }
+    ngOnInit() {
+
+    }
+
+    toggleSidenav() {
+        this.isSideNavStatus = !this.isSideNavStatus;
+    }
+
+    toggleTheme(): void {
+        this.themeService.toggleTheme();
+    }
+
+    onSidenavStateChange(event: boolean) {
+        this.isSideNavStatus = event;
+    }
+
+    logout() {
+        localStorage.removeItem('role');
+        this.roleSubject.next(null); // Clear role on logout
+        this.router.navigate(['/login']);
+    }
+
+    setNavigationItems(role: string | null) {
+        if (role === 'admin') {
+            this.fillerNav = [
+                { name: 'Home', routeLink: '/home', icon: 'home' },
+                { name: 'Add Item', routeLink: 'add-item', icon: 'add' },
+                { name: 'Remove/Archive Item', routeLink: 'remove-archive', icon: 'archive' },
+                { name: 'Search', routeLink: 'search', icon: 'search' },
+            ];
+        } else if (role === 'user') {
+            this.fillerNav = [
+                { name: 'Home', routeLink: '/home', icon: 'home' },
+                { name: 'Search and Claim', routeLink: 'search-claim', icon: 'search' },
+                { name: 'View/Unclaim Item', routeLink: 'view-unclaim', icon: 'visibility' },
+            ];
+        } else {
+            this.fillerNav = [
+                { name: 'Home', routeLink: '/home', icon: 'home' },
+                { name: 'Search and Claim', routeLink: 'search-claim', icon: 'search' },
+                { name: 'View/Unclaim Item', routeLink: 'view-unclaim', icon: 'visibility' },
+            ];
+        }
+    }
 }
